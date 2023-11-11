f014e623c3bb11cd430895f1b9cca44a7d74601c
YASH KUMAR <kyash03@student.ubc.ca>
2023-11-11 12:39:59 -0800

    added router functionality

--- sunscope/src/App.tsx
+++ sunscope/src/App.tsx
@@ -1,11 +1,24 @@
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import NavBar from "./components/NavBar"
+// import Home from "./components/Home"
+// import About from "./components/About"
+// import Estimate from "./components/Estimate"
+
 
 function App() {
 
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>SunScope</h1>
-    </>
+    <Router>
+      {/* <NavBar /> */}
+      <Routes>
+        {/* <Route path="/" element={<Home />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/estimate" element={<Estimate />} /> */}
+        {/* <Route path="/contact" element={<Contact />} /> */}
+        <h1 className='text-3xl font-bold underline'>SunScope</h1>
+      </Routes>
+    </Router>
   )
 }
 
