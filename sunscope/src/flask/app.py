ad00588210212a77a3c6c8f31f414413b9e5446a
Yong Lee <gjftns7220@gmail.com>
2023-11-11 12:56:18 -0700

    update app.py

--- sunscope/src/flask/app.py
+++ sunscope/src/flask/app.py
@@ -1,9 +1,23 @@
-from flask import Flask
+from flask import Flask, request, render_template
+import requests
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, Solar Panel Optimization World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        latitude = request.form['latitude']
+        longitude = request.form['longitude']
+        api_key = "AIzaSyBZMtnp5vEd8vRtZb-XTkk_vfBYA4YeuVc"
+        result = get_solar_insight(latitude, longitude, api_key)
+        return render_template('results.html', result=result)
+    return render_template('index.html')
+
+def get_solar_insight(latitude, longitude, api_key):
+    url = f"https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude={latitude}&location.longitude={longitude}&requiredQuality=HIGH&key={api_key}"
+    response = requests.get(url)
+    return response.json() if response.status_code == 200 else None
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=True)
+    app.run(debug=True)
---
+++ sunscope/src/flask/test.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+def get_solar_insight_and_save(latitude, longitude, api_key, file_path):
+    url = f"https://solar.googleapis.com/v1/buildingInsights:findClosest?location.latitude={latitude}&location.longitude={longitude}&requiredQuality=HIGH&key={api_key}"
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        with open(file_path, 'w') as file:
+            json.dump(response.json(), file)
+        return f"Data saved to {file_path}"
+    else:
+        return f"Error: {response.status_code}"
+
+# Usage
+api_key = "AIzaSyBZMtnp5vEd8vRtZb-XTkk_vfBYA4YeuVc"  # Replace with your actual API key
+latitude = 37.4450  # Replace with your desired latitude
+longitude = -122.1390  # Replace with your desired longitude
+file_path = 'sunscope\src\data\solar_insight.json'  # Specify the path where you want to save the file
+
+result = get_solar_insight_and_save(latitude, longitude, api_key, file_path)
+print(result)
