65eb2e3da0f91ed722598e7e9f4ba3ac2b8ec600
JapmeetBrar <japmeetb@hotmail.com>
2023-11-11 17:58:32 -0500

    Clicking Nav bar now routes

--- sunscope/src/components/NavBar.tsx
+++ sunscope/src/components/NavBar.tsx
@@ -1,31 +1,46 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 enum navItemType {
-    Home = 'Home',
-    About = 'About Us',
-    Contact = 'Contact',
-    Estimate = 'Estimate',
+  Home = "Home",
+  About = "About",
+  Contact = "Contact",
+  Estimate = "Estimate",
 }
-
 
 function NavBar() {
-const [selected, setSelected] = useState(navItemType.Home)
-const navItemsArray = [navItemType.Home, navItemType.About, navItemType.Contact, navItemType.Estimate];
-    
-    return (
-        <div className='h-20 flex gap-20 items-center px-10 py-6 shadow-lg font-roboto rounded-b-lg' >
-            <div className="title text-4xl font-bold text-orange-500">SunScope</div>
-            <div className='grow'>
-                <ul className='flex gap-3 font-bold justify-around text-xl'>
-                    {navItemsArray.map((item) => {
-                        <Link to={'/' + item}></Link>
-                        return <button className={ item === selected ? 'underline decoration-slate-800 underline-offset-8 text-orange-500': 'hover:text-orange-500'} onClick={() => setSelected(item)}>{item}</button>
-                    })}
-                </ul>
-            </div>
-        </div>
-  )
+  const [selected, setSelected] = useState(navItemType.Home);
+  const navItemsArray = [
+    navItemType.Home,
+    navItemType.About,
+    navItemType.Contact,
+    navItemType.Estimate,
+  ];
+
+  return (
+    <div className="h-20 flex gap-20 items-center px-10 py-6 shadow-lg rounded-b-lg">
+      <div className="title text-4xl font-bold text-orange-500">SunScope</div>
+      <div className="grow">
+        <ul className="flex gap-3 font-bold justify-around text-xl">
+          {navItemsArray.map((item) => {
+            return (
+              <Link
+                to={"/" + item.toLowerCase()}
+                className={
+                  item === selected
+                    ? "underline decoration-slate-800 underline-offset-8 text-orange-500"
+                    : "hover:text-orange-500"
+                }
+                onClick={() => setSelected(item)}
+              >
+                {item}
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default NavBar
+export default NavBar;
