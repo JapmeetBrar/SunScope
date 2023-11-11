7f054e8c402902014765ea7708992a1ad261fc83
JapmeetBrar <japmeetb@hotmail.com>
2023-11-11 14:47:51 -0500

    add components folder

--- sunscope/src/components/App.tsx
+++ sunscope/src/components/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>SunScope</h1>
+      <h1 className='text-3xl font-bold underline'>Test</h1>
     </>
   )
 }
