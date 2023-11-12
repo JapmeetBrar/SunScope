3574783097a9520fcd0fe8efce50e3fe30746f86
Yong Lee <gjftns7220@gmail.com>
2023-11-11 21:26:22 -0700

    flask-sidd

--- sunscope/_backend/flask/financial_engineering.py
+++ sunscope/_backend/flask/financial_engineering.py
@@ -1,5 +1,8 @@
 import json
-
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
+import PIL
+from io import BytesIO
 class FinancialEngineering:
     def __init__(self, energy_data):
         self.energy_data = energy_data
@@ -17,34 +20,48 @@ class FinancialEngineering:
         if yearly_revenue == 0:
             return float('inf')  # Infinite payback period if no revenue
         return installation_cost / yearly_revenue
+    def panel_energy_graph(self, file_path):
+        panels = []
+        energy_production = []
+        data = import_json(file_path)
+        for key, energy_kwh in data.items():
+            panels_count = int(key.replace('yearlyEnergyDcKwh', ''))  # Extracting the number of panels
+            panels.append(panels_count)
+            energy_production.append(energy_kwh)
+
+        buffer = BytesIO()
+        plt.xlabel('Number of Panels')
+        plt.ylabel('Energy Production (kWh)')
+        plt.plot(panels, energy_production)
+        plt.savefig(buffer, format='png')
+        img = PIL.Image.open(buffer)
+        saved = img.save('sunscope/_backend/data/PanelEnergyProduction.png')
+        return img
+
 
 def import_json(file_path):
     with open(file_path, 'r') as file:
         data = json.load(file)
     return data
 
-# Path to the uploaded JSON file
-file_path = 'sunscope\_backend\data\get_solar_data_example_CAN_couple.json'
 
-# Importing data from the JSON file
-data = import_json(file_path)
 
-# Creating an instance of FinancialEngineering with the imported data
-fe = FinancialEngineering(data)
 
-# Example rate per kWh and cost per panel for calculations
-rate_per_kwh = 0.10  # This can be adjusted as needed
-cost_per_panel = 1000  # Example: $1000 per panel
+if __name__ == '__main__':
+    # Path to the uploaded JSON file
+    file_path = 'sunscope/_backend/data/get_solar_data_example_CAN_couple.json'
 
-"""# Iterating through each configuration and performing calculations
-for key, energy_kwh in data.items():
-    panels_count = int(key.replace('yearlyEnergyDcKwh', ''))  # Extracting the number of panels
-    revenue = fe.calculate_revenue(panels_count, rate_per_kwh)
-    installation_cost = panels_count * cost_per_panel
-    payback_period = fe.calculate_payback_period(panels_count, rate_per_kwh, installation_cost)
-    
-    print(f"Configuration: {panels_count} Panels")
-    print(f"Yearly Energy Production: {energy_kwh} kWh")
-    print(f"Yearly Revenue: ${revenue:.2f}")
-    print(f"Payback Period: {payback_period:.2f} years")
-    print("-----------------------------------------")"""
+    # Importing data from the JSON file
+    data = import_json(file_path)
+
+    # Creating an instance of FinancialEngineering with the imported data
+    fe = FinancialEngineering(data)
+
+    # Example rate per kWh and cost per panel for calculations
+    rate_per_kwh = 0.10  # This can be adjusted as needed
+    cost_per_panel = 1000  # Example: $1000 per panel
+
+    # Iterating through each configuration and performing calculations
+    fe.panel_energy_graph(file_path)
+
+
