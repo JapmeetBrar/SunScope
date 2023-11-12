4ee572c6a2a58f53bba931ce356e60818b386227
Yong Lee <gjftns7220@gmail.com>
2023-11-12 00:41:18 -0700

    debug_trial

--- sunscope/BE/flask/app.py
+++ sunscope/BE/flask/app.py
@@ -14,10 +14,7 @@ client = MapInfo(googlemaps.Client(API_KEY))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    """
-    Root route to display an HTML form for address input.
-    On form submission, it redirects to the get_coordinates function.
-    """
+    # Root route to display an HTML form for address input
     if request.method == "POST":
         address = request.form.get("address")
         return redirect(url_for("get_coordinates", address=address))
@@ -30,35 +27,30 @@ def index():
 
 @app.route('/coordinates', methods=['GET'])
 def get_coordinates():
-    """
-    Get geographical coordinates for a given address.
-    """
+    # Get geographical coordinates for a given address and fetch solar data
     address = request.args.get('address')
     if address:
         coordinates = client.coordinates(address)
-        return jsonify(coordinates)
+        if coordinates:
+            return redirect(url_for("solar_data", latitude=coordinates[0], longitude=coordinates[1]))
+        return "Coordinates not found", 400
     else:
         return "No address provided", 400
 
 @app.route('/solar_data', methods=['GET'])
 def solar_data():
-    """
-    Endpoint to fetch solar data based on provided coordinates.
-    Expects 'latitude' and 'longitude' as query parameters.
-    """
+    # Fetch solar data based on provided coordinates
     latitude = request.args.get('latitude')
     longitude = request.args.get('longitude')
     if latitude and longitude:
-        data = get_solar_data(latitude, longitude)  # Modify as per your implementation
+        data = get_solar_data(latitude, longitude, API_KEY)
         return jsonify(data)
     else:
         return "Latitude and longitude required", 400
 
 @app.route('/process_json', methods=['POST'])
 def process_json_data():
-    """
-    Endpoint to process JSON data. Expects JSON data in the request body.
-    """
+    # Process JSON data, expects JSON data in the request body
     data = request.json
     processed_data = process_data_custom_keys(data)
     return jsonify(processed_data)
